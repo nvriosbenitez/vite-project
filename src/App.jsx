f9f976d075dc51c82b2a44bf3b64217b0ebb8efa
Valeria <nv.riosbenitez@gmail.com>
2023-11-04 06:06:52 -0300

    vemos frame motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,107 @@
 import { useState } from 'react'
-import {motion} from 'framer-motion'
+//import {motion} from 'framer-motion'
 import './App.css'
+
+
+function App() {
+
+  return (
+
+    <div>
+      <h1>Hola</h1>
+
+    </div>
+
+  )
+}
+
+
+export default App
+
+
+
+
+
+
+
+//const misvariantes={
+  //Podría llarmalo como quiera
+  //hidden:{opacity:[0.1,0.2,0.3,0.9,1],
+  //  x:'100vh'
+//  hidden:{rotate:-100},
+//  visible:{
+//    opacity:1,
+//    x:0,
+//    transition:{  
+//      delay:0.5,
+//      yoyo:10
+    //rotate:0,
+    //transition:{duration:1}
+    //}
+
+//}
+
+/*const pathVariantes={
+  hidden:{
+    opacity:0,
+    pathLength:0
+  },
+  visible:{
+    opacity:1,
+    pathLength:1,
+    transition:{
+      duration:2,
+      ease: "easeInOut"
+    }
+  }
+}
 
 function App() {
   
 
   return (
-    <>
-    <motion.section className='miclase' initial={{opacity:0.2,rotateZ:180}} animate={{opacity:1,rotateZ:0}}>
-      <h2>Hola Mundo</h2>
-   {/* <motion.h2 animate={{fontSize:50,color:'#ff2994',x:100}}>Hola Mundo</motion.h2>*/}
-    
 
-    </motion.section>
-    </>
-   
+  <motion.svg className="pizza-svg" xmlns="http://www.3.org/2000/svg" viewBox="0 0 100 100"
+  variants={misvariantes}
+  initial="hidden"
+  animate="visible"
+  >
+    <motion.path
+      fill="none"
+      d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
+      variants={pathVariantes}
+    />
+    <motion.path
+      fill="none"
+      d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
+      variants={pathVariantes}
+    />
+  </motion.svg>
+  
+
+    //<>esto es el reac.fragment
+//    <motion.div variants={misvariantes} initial="hidden" animate="visible">
+
+//      <motion.section className='miclase' initial={{opacity:0.2,rotateZ:180}} animate={{opacity:1,rotateZ:0}} transition={{delay:1.5,duration:1}}>
+//            <h2>Hola Mundo</h2>
+//      <motion.h2 animate={{fontSize:50,color:'#ff2994',x:100}}>Hola Mundo</motion.h2>
+    
+//      </motion.section>
+//
+//    </motion.div>
+
+    
   )
 }
 
 export default App
+*/
+
+
+
+/*<motion.button whileHover={{scale:1.1,
+      textShadow:"0px 0px 8px rgb(255,255,255)",
+      boxShadow:"0px 0px 8px rgb(255,255,255)"}}>Hace Click</motion.button>
+    */
+    //</>
+   
